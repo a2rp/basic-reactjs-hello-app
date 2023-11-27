3868a7e45cc3c840be834b44be8014b09608f057
ash1198 <ash.ranjan09@gmail.com>
2023-11-27 14:25:31 +0530

    date added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     return (
         <div className="App" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
             a2rp: an Ashish Ranjan presentation
+            <h1>{new Date().toISOString()}</h1>
         </div>
     );
 }
