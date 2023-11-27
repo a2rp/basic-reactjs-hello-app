bea1ba2f0a66d1cb86f0e1d1dc27795f620ff334
ash1198 <ash.ranjan09@gmail.com>
2023-11-27 14:40:29 +0530

    updated date time animation again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [dateTime, setDateTime] = useState(new Date().toISOString());
+    useEffect(() => {
+        const updateDateTime = () => {
+            setDateTime(new Date().toISOString());
+        };
+        const timeout = setTimeout(updateDateTime, 10);
+
+        return () => clearTimeout(timeout);
+    }, [dateTime]);
+
     return (
-        <div className="App" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-            a2rp: an Ashish Ranjan presentation
-            <h1>{new Date().toISOString()}</h1>
+        <div className="App" style={{ display: "flex", gap: "15px", justifyContent: "center", alignItems: "center", height: "100vh", flexDirection: "column" }}>
+            <h1>{dateTime}</h1>
+            <h2>a2rp: an Ashish Ranjan presentation</h2>
         </div>
     );
 }
